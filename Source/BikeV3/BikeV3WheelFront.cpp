da02baa2ad4e7312ebc97d6ca1704d5cbe0d576f
Rune Abrahamsen <runeabrahams1@gmail.com>
2015-12-01 18:25:00 +0100

    Cleanup and improvements. Final version 2015

--- Source/BikeV3/BikeV3WheelFront.cpp
+++ Source/BikeV3/BikeV3WheelFront.cpp
@@ -7,16 +7,16 @@
 UBikeV3WheelFront::UBikeV3WheelFront()
 {
 	ShapeRadius = 36.5f;
-	ShapeWidth = 9.0f;
+	ShapeWidth = 10.0f;
 	bAffectedByHandbrake = false;
 	SteerAngle = 70.0f;
 
 	// Setup suspension forces
 	SuspensionForceOffset = -2.5f;
 	SuspensionMaxRaise = 0.0f;
-	SuspensionMaxDrop = 5.0f;
-	SuspensionNaturalFrequency = 9.0f;
-	SuspensionDampingRatio = 1.05f;
+	SuspensionMaxDrop = 20.0f;
+	SuspensionNaturalFrequency = 36.0f;
+	SuspensionDampingRatio = 10.05f;
 
 	// Find the tire object and set the data for it
 	static ConstructorHelpers::FObjectFinder<UTireType> TireData(TEXT("/Game/Bicycle/WheelData/BicycleTire.BicycleTire"));
